217a65d796cd4b12a9f9fe1cae438cbe584c1e3b
Daniel <danielmesquitadev@gmail.com>
2025-02-01 17:37:31 -0300

    test: create product controller test class

--- src/test/java/dev/danielmesquita/dmcommerce/TestMocks.java
+++ src/test/java/dev/danielmesquita/dmcommerce/TestMocks.java
@@ -7,8 +7,9 @@ import java.util.Set;
 
 public class TestMocks {
 
-  private static final Set<Category> setCategoriesMock =
+  public static final Set<Category> setCategoriesMock =
       new HashSet<>(Set.of(new Category(1L, "Computadores"), new Category(2L, "Tecnologia")));
+
   public static final Product productMock =
       new Product(123L, "PC", "Novo PC", 200.00, "www.image.com", setCategoriesMock);
 }
--- src/test/java/dev/danielmesquita/dmcommerce/controllers/ProductControllerTest.java
+++ src/test/java/dev/danielmesquita/dmcommerce/controllers/ProductControllerTest.java
@@ -1,5 +1,7 @@
 package dev.danielmesquita.dmcommerce.controllers;
 
+import dev.danielmesquita.dmcommerce.TestMocks;
+import dev.danielmesquita.dmcommerce.models.Product;
 import dev.danielmesquita.dmcommerce.services.ProductService;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -10,4 +12,6 @@ public class ProductControllerTest {
   @Mock private ProductService productService;
 
   @InjectMocks private ProductController productController;
+
+  private final Product productMock = TestMocks.productMock;
 }
