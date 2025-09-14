de463dfca94a044eb53101c513a165b6291db962
Daniel <danielmesquitadev@gmail.com>
2025-09-14 20:37:22 -0300

    refactor: remove dead code

--- src/main/java/dev/danielmesquita/dmcommerce/controllers/CategoryController.java
+++ src/main/java/dev/danielmesquita/dmcommerce/controllers/CategoryController.java
@@ -4,8 +4,6 @@ import dev.danielmesquita.dmcommerce.dtos.CategoryDTO;
 import dev.danielmesquita.dmcommerce.services.CategoryService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,12 +19,6 @@ public class CategoryController {
     @GetMapping("/all")
     public ResponseEntity<List<CategoryDTO>> getAllCategories() {
         List<CategoryDTO> categoryDTOS = service.findAll();
-        return ResponseEntity.ok(categoryDTOS);
-    }
-
-    @GetMapping
-    public ResponseEntity<Page<CategoryDTO>> getAllCategories(Pageable pageable) {
-        Page<CategoryDTO> categoryDTOS = service.findAllPageable(pageable);
         return ResponseEntity.ok(categoryDTOS);
     }
 }
